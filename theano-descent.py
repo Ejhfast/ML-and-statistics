5b1addbe960c53a177d5d12ff4acfadbec2a8538
Ethan Fast <ethanfast@rescomp-13-282343.stanford.edu>
2016-08-11 16:56:53 -0700

    multivariate

--- theano-descent.py
+++ theano-descent.py
@@ -4,16 +4,20 @@ import numpy
 
 iterations = 1000
 x_dim = 5
+y_dim = 2
 n_data = 100
 
 # theano data types
 y = T.dmatrix('y')
 X = T.dmatrix('X')
 W = T.dmatrix('W')
-b = T.dscalar('b')
+b = T.dvector('b')
+
+#T.addbroadcast(b,0)
 
 # sum of squares cost function for linear regression
 cost = T.sum((y-(T.dot(X,W)+b))**2)
+# for L1 or L2 regularization, add T.sum(W**2) or T.sum(|W|)
 
 # compile executable cost
 run_cost = theano.function([y,X,W,b],cost)
@@ -30,8 +34,8 @@ db_cost = theano.function([y,X,W,b],gbcost)
 Xt = numpy.matrix(numpy.random.rand(n_data,x_dim))
 
 # randomly choose parameters that we will later search for
-W_goal = numpy.matrix(numpy.random.rand(x_dim,1))
-b_goal = numpy.random.randn()
+W_goal = numpy.matrix(numpy.random.rand(x_dim,y_dim))
+b_goal = numpy.random.rand(y_dim)
 
 # generate y-data based on X and parameters
 yt = Xt*W_goal+b_goal
@@ -41,8 +45,8 @@ print("weights:", W_goal)
 print("b:", b_goal)
 
 # initialize search at zero for params
-bt = 0
-Wt = numpy.matrix(numpy.zeros((x_dim,1)))
+bt = numpy.zeros(y_dim)
+Wt = numpy.matrix(numpy.zeros((x_dim,y_dim)))
 
 learning_rate = 0.001
 
