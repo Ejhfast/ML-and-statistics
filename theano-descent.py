2c1feb8119d9f019d86ff8bdbdb2c28c158155a8
Ethan Fast <ethanfast@rescomp-13-282343.stanford.edu>
2016-08-11 20:39:54 -0700

    collapse b parameter into W

--- theano-descent.py
+++ theano-descent.py
@@ -4,56 +4,47 @@ import numpy
 
 iterations = 1000
 x_dim = 5
-y_dim = 2
+y_dim = 1
 n_data = 100
 
 # theano data types
 y = T.dmatrix('y')
 X = T.dmatrix('X')
 W = T.dmatrix('W')
-b = T.dvector('b')
-
-#T.addbroadcast(b,0)
 
 # sum of squares cost function for linear regression
-cost = T.sum((y-(T.dot(X,W)+b))**2)
+cost = T.sum((y-T.dot(X,W))**2)
 # for L1 or L2 regularization, add T.sum(W**2) or T.sum(|W|)
 
 # compile executable cost
-run_cost = theano.function([y,X,W,b],cost)
+run_cost = theano.function([y,X,W],cost)
 
-# get gradients in terms of W (parameter weights) and b
+# get gradients in terms of W (parameter weights)
 gwcost = T.grad(cost,W)
-gbcost = T.grad(cost,b)
 
-# executable derivatives from the gradients
-dw_cost = theano.function([y,X,W,b],gwcost)
-db_cost = theano.function([y,X,W,b],gbcost)
+# executable derivative from the gradients
+d_cost = theano.function([y,X,W],gwcost)
 
-# create test X data
-Xt = numpy.matrix(numpy.random.rand(n_data,x_dim))
+# create test X data (add another 1s column for b parameter)
+Xt = numpy.matrix(numpy.random.rand(n_data,x_dim+1))
+Xt[:,x_dim] = 1 
 
-# randomly choose parameters that we will later search for
-W_goal = numpy.matrix(numpy.random.rand(x_dim,y_dim))
-b_goal = numpy.random.rand(y_dim)
+# randomly choose parameters that we will later search for (x_dim+1 for b parameter)
+W_goal = numpy.matrix(numpy.random.rand(x_dim+1,y_dim))
 
 # generate y-data based on X and parameters
-yt = Xt*W_goal+b_goal
+yt = Xt*W_goal
 
 print("searching for:")
 print("weights:", W_goal)
-print("b:", b_goal)
 
 # initialize search at zero for params
-bt = numpy.zeros(y_dim)
-Wt = numpy.matrix(numpy.zeros((x_dim,y_dim)))
+Wt = numpy.matrix(numpy.zeros((x_dim+1,y_dim)))
 
 learning_rate = 0.001
 
 for i in range(0,iterations):
-    w_update = dw_cost(yt,Xt,Wt,bt)
-    b_update = db_cost(yt,Xt,Wt,bt)
+    w_update = d_cost(yt,Xt,Wt)
     Wt = Wt - (w_update*learning_rate)
-    bt = bt - (b_update*learning_rate)
 print("After {} iterations found:".format(iterations))
-print(Wt,bt)
+print(Wt)
